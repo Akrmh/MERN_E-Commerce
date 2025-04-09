d508dd2eea43e2134df07d23b18314874a82975f
Akrmh <akrmh90@gmail.com>
2025-04-09 18:31:40 +0200

    update

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "sandbox",
-  client_id: "AfN9TuPObcFYF5e6gay9iDZGnuEat_Y0r8DWR-qN2QSM5mt8EEti7i8sQyWCd7fDFdRHzR0ePCdjSwfz",
-  client_secret: "EN85MRAK72unDF021n53DVPVohkMZz2nvcFGZdxjpElmV356ltWeuHPqgKWkauv-LFUu8UwlatYBFeyb",
+  mode: process.env.PAYPAL_Mode,
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
 module.exports = paypal;
