9cf45858cf28393398ce649b7f0257eea6b8fce3
Akrmh <akrmh90@gmail.com>
2025-04-12 15:46:26 +0200

    update backend url

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -18,7 +18,8 @@ export const fetchAllFilteredProducts = createAsyncThunk(
     });
 
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get?${query}`
+       `${import.meta.env.VITE_BACKEND_URL}/api/shop/products/get?${query}`
+      // `https://mern-e-commerce-6ph8.onrender.com/api/shop/products/get?${query}`
     );
 
     console.log(result);
@@ -31,7 +32,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get/${id}`
+      `${import.meta.env.VITE_BACKEND_URL}/api/shop/products/get/${id}`
     );
 
     return result?.data;
